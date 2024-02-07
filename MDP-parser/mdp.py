8afe748281311a166ff3f15d36a582a8515e75c1
furarox <110490729+furarox@users.noreply.github.com>
2024-02-07 15:36:55 +0100

    Add files via upload

---
+++ MDP-parser/mdp.py
@@ -0,0 +1,48 @@
+from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
+from gramLexer import gramLexer
+from gramListener import gramListener
+from gramParser import gramParser
+import sys
+
+
+class gramPrintListener(gramListener):
+
+    def __init__(self):
+        pass
+
+    def enterDefstates(self, ctx):
+        print("States: %s" % str([str(x) for x in ctx.ID()]))
+
+    def enterDefactions(self, ctx):
+        print("Actions: %s" % str([str(x) for x in ctx.ID()]))
+
+    def enterTransact(self, ctx):
+        ids = [str(x) for x in ctx.ID()]
+        dep = ids.pop(0)
+        act = ids.pop(0)
+        weights = [int(str(x)) for x in ctx.INT()]
+        print("Transition from " + dep + " with action " + act +
+              " and targets "
+              + str(ids) + " with weights " + str(weights))
+
+    def enterTransnoact(self, ctx):
+        ids = [str(x) for x in ctx.ID()]
+        dep = ids.pop(0)
+        weights = [int(str(x)) for x in ctx.INT()]
+        print("Transition from " + dep + " with no action and targets "
+              + str(ids) + " with weights " + str(weights))
+
+
+def main(argv):
+    input_stream = FileStream(argv[1])
+    lexer = gramLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = gramParser(stream)
+    tree = parser.program()
+    printer = gramPrintListener()
+    walker = ParseTreeWalker()
+    walker.walk(printer, tree)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
